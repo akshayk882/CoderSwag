66b1a0081ac9ca9a0e81980c8130e96722db039c
akshay <akshayk882.com>
2018-12-31 23:49:45 +0530

    Recycle View is implemented

--- app/src/main/java/com/akshaykumbhar/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/akshaykumbhar/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,11 @@ package com.akshaykumbhar.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.appcompat.R.styleable.RecycleListView
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.ArrayAdapter
+import com.akshaykumbhar.coderswag.Adapter.CategoryRecycleAdapter
 import com.akshaykumbhar.coderswag.Adapter.categoryadpter
 import com.akshaykumbhar.coderswag.Model.Category
 import com.akshaykumbhar.coderswag.R
@@ -11,11 +15,16 @@ import com.akshaykumbhar.coderswag.Service.Dataservice.categories
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter : categoryadpter
+    lateinit var adapter : CategoryRecycleAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = categoryadpter(this, Dataservice.categories)
-        categorylist.adapter = adapter
+        adapter = CategoryRecycleAdapter(this,Dataservice.categories){category->
+
+        }
+        recycleview.adapter = adapter
+        val layoutmanager = LinearLayoutManager(this)
+        recycleview.layoutManager = layoutmanager
+        recycleview.setHasFixedSize(true)
     }
 }
